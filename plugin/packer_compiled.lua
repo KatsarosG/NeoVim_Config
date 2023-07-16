00a446790b5f421efa47599caefe61fbb99469d4
KatsarosG <giorgoskgkg@gmail.com>
2023-07-16 16:17:36 +0300

    Test commit on macos

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -76,22 +76,22 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/home/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/Users/giorgoskatsaros/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/home/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/giorgoskatsaros/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/home/home/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/Users/giorgoskatsaros/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "/home/home/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    path = "/Users/giorgoskatsaros/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   ["lualine.nvim"] = {
@@ -111,7 +111,7 @@ _G.packer_plugins = {
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/home/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/giorgoskatsaros/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
